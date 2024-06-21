0e6fcb370c65a636b2ca31fe1e497bbed5e193e8
Zahra <zahramoosanni@icloud.com>
2024-06-21 12:22:10 -0500

    Initial Commit

---
+++ twoinputs/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  twoinputs
+//
+//  Created by Zahra Moosani on 6/21/24.
+// Shipbuilder app - two names combine to make one duo
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name1 = " "
+    @State private var name2 = " "
+    @State private var textTitle = " "
+    var body: some View {
+        VStack{
+            
+            Text ("What is your name?")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            TextField(" ", text: $name1)
+                .multilineTextAlignment(.center)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+            
+            Text ("What is your friends name?")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            TextField(" ", text: $name2)
+                .multilineTextAlignment(.center)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 2)
+            
+            Button("Submit Names") {
+                textTitle = "Your duo name is \(name1 + name2)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+        
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ twoinputs/twoinputsApp.swift
@@ -0,0 +1,17 @@
+//
+//  twoinputsApp.swift
+//  twoinputs
+//
+//  Created by Zahra Moosani on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct twoinputsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
